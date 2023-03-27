6c3f9451b755570a284245d87788eafa980542ce
Raphaël Faure <raphael.faure2@student-cs.fr>
2023-03-27 09:54:23 +0200

    Test and working

--- src/core/packet_context.rs
+++ src/core/packet_context.rs
@@ -1,13 +1,26 @@
-use std::{time::Duration, net::{IpAddr, Ipv4Addr}, vec, io::Read};
-
+use std::{time::Duration, net::Ipv4Addr};
 use mac_address::MacAddress;
 
-pub trait Packet {
+use super::state;
+
+pub struct Packet<T> {
+    pub state : state::PacketState,
+    pub packet : T
 }
-pub struct PacketContext<T : Packet, U> {
+
+pub trait PacketType {
+
+}
+
+impl PacketType for Packet<DhcpPacket>{
+}   
+
+
+pub struct PacketContext<T : PacketType, U> {
     pub input_packet : T,
     pub output_packet : U
 }
+
 
 pub struct DhcpPacket {
     pub htype : u8,
@@ -26,8 +39,10 @@ pub struct DhcpPacket {
     pub options : Vec<Option<Vec<u8>>>
 }
 
+
 impl DhcpPacket {
     pub fn from_raw(mut raw : Vec<u8>) -> Self{
+        let _mtype = raw.remove(0);
         let htype = raw.remove(0);
         let hlen = raw.remove(0);
         let hops = raw.remove(0);
@@ -59,9 +74,10 @@ impl DhcpPacket {
         let next: [u8; 16] = raw.drain(0..16).as_slice().to_owned().try_into().unwrap();
         let chadd = HardwareAddress::new(next);
         let next = raw.drain(0..64).as_slice().to_vec();
-        let sname = String::from_utf8(next).unwrap();
+        let sname = String::from_utf8_lossy(&next).to_string();
         let next = raw.drain(0..128).as_slice().to_vec();
-        let file = String::from_utf8(next).unwrap();
+        let file = String::from_utf8_lossy(&next).to_string();
+        let _magic_cookie = raw.drain(0..4).as_slice().to_vec();
         let options = parse_options(raw.to_owned());
         Self { htype: htype, hlen: hlen, hops: hops, xid: xid, sec: sec, flags: flags, ciaddr: ciaddr, yiaddr: yiaddr, siaddr: siaddr, giaddr: giaddr, chadd: chadd, sname: sname, file: file, options: options }
 
@@ -76,15 +92,17 @@ pub struct HardwareAddress {
 }
 
 impl HardwareAddress {
-    pub fn new(raw : [u8; 16]) -> Self{
+    pub fn new(mut raw : [u8; 16]) -> Self{
         let mut i =0;
-        while (*raw.get(i).unwrap() != 0u8) & (i < 10) {
+        raw.reverse();
+        while (raw.get(i).unwrap().to_owned() == 0) && (i < 9) {
             i+=1
         }
+        raw.reverse();
         let mut addr = MacAddress::new([0; 6]);
         let mut is_mac_addres = false;
         if i == 9 {
-            let bytes : [u8;6] = raw[9..15].try_into().unwrap();
+            let bytes : [u8;6] = raw[0..6].try_into().unwrap();
             addr = MacAddress::new(bytes);
             is_mac_addres = true;
         }
@@ -92,6 +110,8 @@ impl HardwareAddress {
         
     }
 }
+
+#[derive(Debug)]
 pub struct Option<T> {
     pub code : u8,
     pub value : T
@@ -112,4 +132,32 @@ pub fn parse_options(mut data : Vec<u8>) -> Vec<Option<Vec<u8>>>{
         result.push(Option{code, value});
     }
     result
+}
+
+#[cfg(test)]
+
+
+mod packet {
+    use super::DhcpPacket;
+    #[test]
+    fn packet_craft(){
+        let data = hex_to_bytes("02010601fb2ea0b400000000000000008ac33c830000000000000000b0be8328430e00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000638253633501053604c00002013304000062dc0104fffff00003048ac3300106088ac3240a8ac3c2170f1b6f70656e7a6f6e652e63656e7472616c65737570656c65632e6672771f086f70656e7a6f6e650f63656e7472616c65737570656c656302667200c009ff00000000").unwrap();
+        let packet = DhcpPacket::from_raw(data);
+        println!("{}", packet.chadd.address);
+        println!("{}", packet.yiaddr);
+        dbg!("{}", packet.options);
+    }
+
+    fn hex_to_bytes(s: &str) -> Option<Vec<u8>> {
+        if s.len() % 2 == 0 {
+            (0..s.len())
+                .step_by(2)
+                .map(|i| s.get(i..i + 2)
+                          .and_then(|sub| u8::from_str_radix(sub, 16).ok()))
+                .collect()
+        } else {
+            None
+        }
+    }
+
 }
