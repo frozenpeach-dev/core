fdfbf8d6dafe16a15c1969c4e2c2d7460e1ea5fa
Raphaël Faure <raphael.faure2@student-cs.fr>
2023-03-30 16:59:33 +0200

    Sus test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 pub mod core;
 pub mod hooks;
 pub mod utils;
+use std::sync::{Mutex, Arc};
 
-fn main() {
-    println!("Hello, world!");
+
+use mysql::params;
+use tokio;
+
+use utils::data::DbManager;
+
+
+#[tokio::main]
+async fn main() {
+    let db = Arc::new(Mutex::new(DbManager::new(String::from("dhcp"), String::from("frozenpeach"), String::from("poney"), String::from("127.0.0.1:3333"))));
+    let database = db.clone();
+    tokio::spawn(async move {
+        let db = database.lock().unwrap();
+        db.exec(String::from("INSERT INTO lease VALUE (:address, :name, :id)"), params! {"address" => "127.0.0.1", "name" => "frost", "id" => 3}).await.unwrap().unwrap();
+    });
 }
