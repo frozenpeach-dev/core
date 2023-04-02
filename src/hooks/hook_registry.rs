84eb065f681030d1c8acf394a745fb5f15e3da9d
Sacha <sacha.dupuydauby@student-cs.fr>
2023-04-02 16:21:32 +0200

    Function documentation

--- src/hooks/hook_registry.rs
+++ src/hooks/hook_registry.rs
@@ -36,22 +36,43 @@ impl<T: PacketType + Send, U: PacketType + Send> Hook<T, U> {
     /// [`TypeMap`] enclosed in an [`Arc`] and [`Mutex`]
     /// and a mutable reference to a [`PacketContext`]
     ///
+    /// It must return a [`Result`], which can be either a [`i32`] number, positive in case of
+    /// success, negative otherwise, or a [`HookError`]
     /// # Examples:
     ///
     /// ```
-    /// let my_hook = Hook::new("My first hook", Box::new(|services, packet| { println!(packet.id); }));
+    /// let my_hook = Hook::new("My hook", Box::new(|services, packet| { println!(packet.id); }));
     /// ```
     pub fn new(name: String, exec: Box<dyn Fn(Arc<Mutex<TypeMap>>, &mut PacketContext<T, U>) -> Result<isize, HookError>>, flags: Vec<HookFlags>) -> Self {
         let id = Uuid::new_v4();
         Self {id, name, dependencies: HashMap::new(), exec, flags, next_hook: None}
     }
 
-    /// Add a dependency to the success of another [`Hook`] specified by its [`Uuid`]
+    /// Add a dependency to the success of another `Hook` specified by its [`Uuid`]
     ///
+    /// # Examples:
+    ///
+    /// ```
+    /// let my_hook = Hook::new("Hook1", Box::new(|_, _| { }));
+    /// let dependent_hook = Hook::new("Hook2", Box::new(|_, _| {} ));
+    ///
+    /// dependent_hook.must(my_hook.id);
+    /// ```
+
     pub fn must(&mut self, hook: Uuid) {
         self.dependencies.insert(hook, true);
     }
 
+    /// Add a dependency to the failure of another `Hook` specified by its [`Uuid`]
+    ///
+    /// # Examples:
+    ///
+    /// ```
+    /// let my_hook = Hook::new("Hook1", Box::new(|_, _| { }));
+    /// let dependent_hook = Hook::new("Hook2", Box::new(|_, _| {} ));
+    ///
+    /// dependent_hook.must(my_hook.id);
+    /// ```
     pub fn must_not(&mut self, hook: Uuid) {
         self.dependencies.insert(hook, false);
     }
@@ -60,19 +81,50 @@ impl<T: PacketType + Send, U: PacketType + Send> Hook<T, U> {
 
 impl<T: PacketType + Send, U: PacketType + Send> HookRegistry<T, U> {
 
+    /// Creates a new `HookRegistry`
+    ///
+    /// This does not allocate initial buffers for 
+    /// the underlying registries for [`Hook`] or services 
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let registry = HookRegistry::new();
+    /// ```
     pub fn new() -> Self {
         Self { registry: HashMap::new(), services: Arc::new(Mutex::new(TypeMap::new()))}
     }
 
-    pub async fn run_hooks(&self, packet: &mut packet_context::PacketContext<T, U>) -> Result<(), HookError> {
+    /// Execute every registered [`Hook`] on the given [`PacketContext`] 
+    /// for its current state
+    ///
+    /// # Errors
+    ///
+    /// Returns [`HookError`] if any [`Hook`] holding the [`Fatal`]
+    /// flag panics.
+    ///
+    /// [`Fatal`]: crate::hooks::flags::HookFlags::Fatal
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut registry = HookRegistry::new();
+    /// let my_hook = Hook::new("My hook", Box::new(|services, packet| { println!(packet.id); }));
+    /// registry.register_hook(PacketState::Received, my_hook);
+    /// let mut packet: PacketContext<A, A> = PacketContext::new(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 1), 1, input_packet);
+    /// 
+    /// registry.run_hooks(packet);
+    /// ```
+    ///
+    /// This will print out a 1
+    pub fn run_hooks(&self, packet: &mut packet_context::PacketContext<T, U>) -> Result<(), HookError> {
     
         let mut exec_code: HashMap<Uuid, isize> = HashMap::new();        
         if packet.state() == &PacketState::Failure {
-            self._run_failure_chain(packet)?
+            self.run_failure_chain(packet)?
         }
 
         let exec_order = self.generate_graph(&packet.state())?;
-        dbg!(exec_order.clone());
 
         for hook in exec_order.iter() {
 
@@ -80,7 +132,7 @@ impl<T: PacketType + Send, U: PacketType + Send> HookRegistry<T, U> {
 
             if exec_code.contains_key(&hook.id) { continue; }
 
-            if self._can_execute(&exec_code, &hook.dependencies) {
+            if self.can_execute(&exec_code, &hook.dependencies) {
                 (hook.exec)(self.services.clone(), packet)
                     .and_then(|x| {
                         exec_code.insert(hook.id, x);
@@ -88,7 +140,7 @@ impl<T: PacketType + Send, U: PacketType + Send> HookRegistry<T, U> {
                         Ok(())
                     })
                     .or_else(|_| {
-                        if hook.flags.contains(&HookFlags::Fatal) { return self._run_failure_chain(packet); }
+                        if hook.flags.contains(&HookFlags::Fatal) { return self.run_failure_chain(packet); }
                         else { 
                              exec_code.insert(hook.id, -1);
                              debug!("Hook {} exited with failure (exit code -1)", hook.name);
@@ -101,12 +153,42 @@ impl<T: PacketType + Send, U: PacketType + Send> HookRegistry<T, U> {
 
 
         }
-
         Ok(())
+    }
+
+    /// Insert a new [`Hook`] inside the [`HookRegistry`] 
+    /// for a given [`PacketState`]
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut registry = HookRegistry::new();
+    /// let my_hook = Hook::new("My hook", Box::new(|_, _| { }));
+    /// registry.register_hook(PacketState::Received, my_hook);
+    /// ```
+
+    pub fn register_hook(&mut self, state: PacketState, hook: Hook<T,U> ) {
+
+        if self.registry.contains_key(&state) {
+            self.registry.get_mut(&state).unwrap().insert(hook.id, hook);
+
+        } else {
+            self.registry.insert(state, HashMap::new());
+            self.register_hook(state, hook);
+        }
 
     }
 
-    fn _run_failure_chain(&self, packet: &mut PacketContext<T, U>) -> Result<(), HookError> {
+    /// Insert a new service inside the [`HookRegistry`]
+    ///
+    /// The service's type must implement the following traits:
+    /// [`Send`] and [`Sync`]
+
+    pub fn register_service<V: Send + Sync + 'static>(&mut self, service: V) {
+        self.services.try_lock().unwrap().insert(Arc::new(service));
+    }
+
+    fn run_failure_chain(&self, packet: &mut PacketContext<T, U>) -> Result<(), HookError> {
         
         for hook in self.registry.get(&PacketState::Failure).unwrap().values() {
             (hook.exec)(self.services.clone(), packet)
@@ -119,7 +201,7 @@ impl<T: PacketType + Send, U: PacketType + Send> HookRegistry<T, U> {
 
     }
 
-    fn _can_execute(&self, exec_code: &HashMap<Uuid, isize>, dependencies: &HashMap<Uuid, bool>) -> bool {
+    fn can_execute(&self, exec_code: &HashMap<Uuid, isize>, dependencies: &HashMap<Uuid, bool>) -> bool {
 
         !dependencies.iter().any(|(x, need_success)| {
             exec_code.get(x)
@@ -129,23 +211,6 @@ impl<T: PacketType + Send, U: PacketType + Send> HookRegistry<T, U> {
                 .unwrap_or_default()
         })
 
-    }
-
-    pub fn register_hook(&mut self, state: PacketState, hook: Hook<T,U> ) {
-
-        if self.registry.contains_key(&state) {
-            dbg!(hook.id.clone());
-            self.registry.get_mut(&state).unwrap().insert(hook.id, hook);
-
-        } else {
-            self.registry.insert(state, HashMap::new());
-            self.register_hook(state, hook);
-        }
-
-    }
-
-    pub fn register_service<V: Send + Sync + 'static>(&mut self, service: V) {
-        self.services.try_lock().unwrap().insert(Arc::new(service));
     }
 
     fn generate_graph(&self, for_state: &PacketState) -> Result<Vec<Uuid>, HookError>{
@@ -192,7 +257,6 @@ mod tests {
     use std::net::{SocketAddr, IpAddr, Ipv4Addr};
 
     use super::*;
-    use tokio_test;
     struct A {
         name: usize
     }
@@ -228,7 +292,7 @@ mod tests {
         let mut packet: PacketContext<A, A> = PacketContext::new(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 1), 1, input_packet);
 
         assert!(packet.output_packet.name == 0);
-        tokio_test::block_on(registry.run_hooks(&mut packet)).unwrap();
+        registry.run_hooks(&mut packet).unwrap();
         assert!(packet.output_packet.name == 2);
 
     }
@@ -252,7 +316,7 @@ mod tests {
         let mut packet: PacketContext<A, A> = PacketContext::new(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 1), 1, input_packet);
         registry.register_hook(PacketState::Received, hook2);
         registry.register_hook(PacketState::Received, hook3);   
-        tokio_test::block_on(registry.run_hooks(&mut packet)).unwrap();
+        registry.run_hooks(&mut packet).unwrap();
     }
 
     #[test]
@@ -273,9 +337,14 @@ mod tests {
 
         let mut packet: PacketContext<A, A> = PacketContext::new(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 1), 1, input_packet);
 
-        tokio_test::block_on(registry.run_hooks(&mut packet)).unwrap();
+        registry.run_hooks(&mut packet).unwrap();
         assert!(registry.services.try_lock().unwrap().get::<Arc<Mutex<TestService>>>().unwrap().try_lock().unwrap().list.len() == 2);
 
+    }
+
+    #[test]
+    fn test_dependency_tree() {
+        let mut registry: HookRegistry<A, A> = HookRegistry::new();
     }
 
 }
