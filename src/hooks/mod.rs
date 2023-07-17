3523c3b97007650f317d81555b595c44c0fd35f7
Sacha Dupuydauby <gh.4eczg@slmail.me>
2023-07-18 01:27:34 +0200

    :art: Code quality and minor fixes (#13)

--- src/hooks/mod.rs
+++ src/hooks/mod.rs
@@ -1,3 +1,3 @@
-mod typemap;
 pub mod flags;
 pub mod hook_registry;
+pub mod typemap;
--- src/netio/udp_output.rs
+++ src/netio/udp_output.rs
@@ -1,22 +1,21 @@
-//! Simple [`Output`] implementation using the 
+//! Simple [`Output`] implementation using the
 //! UDP protocol. It reads bytes from a [`PacketType`]
 //! by calling `to_raw_bytes`, and turns these into
 //! a UDP packet.
-use std::net::{SocketAddrV4, Ipv4Addr};
+use std::net::{Ipv4Addr, SocketAddrV4};
 
 use async_trait::async_trait;
 use tokio::net::UdpSocket;
 
-use crate::core::{state_switcher::Output, packet::PacketType};
+use crate::core::{packet::PacketType, state_switcher::Output};
 
-/// `UdpOutput` provides a simple implementation of 
+/// `UdpOutput` provides a simple implementation of
 /// an [`Output`] using the UDP protocol.
-struct UdpOutput{
+struct UdpOutput {
     socket: UdpSocket,
 }
 
 impl UdpOutput {
-
     /// Binds the `UdpOutput` listener to the provided address
     ///
     /// # Examples:
@@ -26,9 +25,10 @@ impl UdpOutput {
     /// ```
 
     pub async fn start(addr: &str) -> Result<Self, std::io::Error> {
-       Ok(Self { socket: UdpSocket::bind(addr).await? })
+        Ok(Self {
+            socket: UdpSocket::bind(addr).await?,
+        })
     }
-
 }
 
 #[async_trait]
@@ -37,12 +37,13 @@ impl<T: PacketType + Sync + Send + 'static> Output<T> for UdpOutput {
     async fn send(&self, packet: T) -> Result<usize, std::io::Error> {
         let raw_bytes = packet.to_raw_bytes();
         if let Some(addr) = &raw_bytes.get(..6) {
-            let addr = SocketAddrV4::new(Ipv4Addr::new(addr[0], addr[1], addr[2], addr[3]), ((addr[4] as u16) << 8) | addr[5] as u16);
-
+            let addr = SocketAddrV4::new(
+                Ipv4Addr::new(addr[0], addr[1], addr[2], addr[3]),
+                ((addr[4] as u16) << 8) | addr[5] as u16,
+            );
             self.socket.send_to(&raw_bytes[6..], addr).await
         } else {
-            return Ok(0)
-        } 
-        
+            return Ok(0);
+        }
     }
 }
